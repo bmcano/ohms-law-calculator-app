96b57923ae34177f78120ed3720d4ef1c09650b2
Brandon Cano <brandoncano01@gmail.com>
2025-01-12 12:24:28 -0600

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin)
+    alias(libs.plugins.jetbrains.kotlin.compose)
+}
+
+android {
+    namespace = "com.brandoncano.ohmslawcalculator"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.brandoncano.ohmslawcalculator"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1 // for 1.0.0-RC1
+        versionName = "1.0.0-develop"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+    applicationVariants.configureEach {
+        resValue("string", "version", versionName)
+        resValue("string", "last_updated", "1/11/2025")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    @Suppress("UnstableApiUsage")
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+composeCompiler {
+    enableStrongSkippingMode = true
+}
+
+dependencies {
+    // androidx
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    // androidx.compose.ui
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    // androidx.compose
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.compose.material3)
+    // com.google
+    implementation(libs.gson)
+    // unit testing
+    testImplementation(libs.mockk)
+    testImplementation(libs.junit)
+    // external libraries
+    implementation(libs.bmcano.util)
+}
